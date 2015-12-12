d85a1bce3bd25b1d70f96d205f244b81d15fb58e
Stephen Ciauri <stephen.ciauri@gmail.com>
2015-12-12 01:17:35 -0800

    calendar picker and config mod

---
+++ sample_config.py
@@ -0,0 +1,36 @@
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+print(SQLALCHEMY_DATABASE_URI)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_CONNECT_OPTIONS = {}
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = "xxx"
+
+# Secret key for signing cookies
+SECRET_KEY = "xxx"
+
+# Twitter / Tweepy Keys
+CONSUMER_KEY = "xxx"
+CONSUMER_SECRET = "xxx"
+ACCESS_TOKEN = "xxx"
+ACCESS_TOKEN_SECRET = "xxx"
